329d907ac589c0c5f0e39f5c4547e7adc2ccaa75
Gebeyaw <gebeyaw.b81@gmail.com>
2025-09-19 20:52:01 +0300

    header and footer

---
+++ netflix-clone/src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../Componenets/Header/Header'
+import Footer from '../../Componenets/Header/Footer/Footer'
+
+const Home = () => {
+  return (
+    <>
+        <Header />
+        <Footer />
+    </>
+  )
+}
+
+export default Home
