760e3f2479ffaeef7019c55babcd69b5f36ad38f
Gebeyaw Belayneh <gebeyaw.b81@gmail.com>
2025-10-07 14:43:12 +0300

    row

--- netflix-clone/src/Componenets/Rows/RowList/RowList.js
+++ netflix-clone/src/Componenets/Rows/RowList/RowList.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import Row from '../Row/Row'
-function RowList() {
+import requests from '../../../utils/requests'
+const RowList = () => {
   return (
     <>
-      <Row />
-      <Row />
-      <Row />
-      <Row />
-      <Row />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="TV Shows" fetchUrl={requests.fetchTvShow} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </>
   );
 }
